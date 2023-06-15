a4ca3f79ea0fe2c410a4f4326a72b378f31b6088
gozde-apk <gozde.apak@hotmail.com>
2023-06-15 18:22:43 +0300

    S13G1 TAMAMLANDI

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const server = express();
 const port = 9000;
 const userModel = require("./users/model")
 
-
+server.use(express.json());
 server.get("/",function(req, res){
     res.send("hello world");
 } );
@@ -16,6 +16,7 @@ server.listen(port, function(err){
 })
 
 server.post("/api/users", async (req,res)=>{
+
      try {
         let {name,bio} = req.body;
      if(!name || !bio){  /*name ve bio alanı yoksa*/
@@ -50,5 +51,72 @@ server.post("/api/users", (req,res)=>{
     }
 });
 */
+
+
+
+server.get("/api/users",  async (req,res) =>{
+    try {
+    let allUsers = await userModel.find()
+    res.json(allUsers);
+    } catch (err){
+        res.status(500).json({
+            message:"Kullanıcı bilgileri bulunamadı"
+        })
+    }
+});
+
+
+
+server.get("/api/users/:id" , async (req,res) => {
+    try{
+        let user = await userModel.findById(req.params.id)
+        if(!user){
+            res.status(404).json({message:"Kullanıcı bilgileri bulunamadı"});
+        }else{
+            res.json(user);
+        }
+    } catch (err){
+     res.status(500).json({
+        message:"kullanıcı bilgileri bulunamadı"
+     })
+    }
+})
+
+server.delete("/api/users/:id" ,async (req,res) => {
+    try{
+        let user = await userModel.findById(req.params.id)
+        if(!user){
+            res.status(404).json({message:"Belirtilen İd li kullanıcı bulunamadı"});
+        }else{
+           await userModel.remove(req.params.id);
+           res.json(user)
+        }
+    } catch (err){
+     res.status(500).json({
+        message:"kullanıcı silinemedi"
+     })
+    }
+})
+
+
+server.put("/api/users/:id" ,async (req,res) => {
+    try{
+        let user = await userModel.findById(req.params.id)
+        if(!user){
+            res.status(404).json({message:"Belirtilen İd li kullanıcı bulunamadı"});
+        }else{
+          let {name,bio} =req.body
+        if(!name || !bio)  {
+            res.status(400).json({message:"Lütfen kullanıcı için bir name ve bio sağlayın"})
+        }else{
+            let updatedUser = await userModel.update(req.params.id,{name,bio});
+            res.json(updated)
+        } }
+    } catch (err){
+     res.status(500).json({
+        message:"kullanıcı silinemedi"
+     })
+    }
+})
 
 module.exports = server; // SERVERINIZI EXPORT EDİN {}
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const server = require('./api/server');
 
 const port = 9000;
+console.log("hello world....")
+
+
 
 // START YOUR SERVER HERE
